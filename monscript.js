2745ba8ddef61bdd1f370c8ec30e7b57df6f2b6c
Tagorier <119063140+Tagorier@users.noreply.github.com>
2024-10-05 09:42:22 +0200

    Update monscript.js

--- monscript.js
+++ monscript.js
@@ -2,10 +2,11 @@
 var scrollSpeed = 100;
 var current = 0;
 
-function bgscroll() {
-  current += 50;
-  $('body').css("backgroundPosition", current + "px " + current/2 +"px");
-}
-
-setInterval("bgscroll()", scrollSpeed);
+$(function(){
+    var x = 0;
+    setInterval(function(){
+        x-=1;
+        $('body').css('background-position', x + 'px 0');
+    }, scrollSpeed);
+})
 
