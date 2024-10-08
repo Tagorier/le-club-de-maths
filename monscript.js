a0bca18e53915fba2a09ad1890071129d5b3e910
Tagorier <119063140+Tagorier@users.noreply.github.com>
2024-10-08 09:23:50 +0200

    Delete monscript.js

--- monscript.js
+++
@@ -1,8 +0,0 @@
-fileStream.readFile( "code3/forwho" , "utf8" , function(error , file){
-     if(error) {
-          logWriter.writeInLog("Une erreur à été remontée : " + error , fileStream);
-     }else{
-          response.writeHead(200 , {"Content-Type" : "text/html" }); 
-          response.write(file);
-          response.end();
-     }		
